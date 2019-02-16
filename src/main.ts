d6eb1cfc718e3ed669c7fba60e76eabf6a08e686
Jacob Martin <mindnumb@gmail.com>
2019-02-15 23:34:55 -0800

    Working hot reloading

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,15 @@ import './main.scss';
 let app = new PIXI.Application({width: 256, height: 256});
 
 //Add the canvas that Pixi automatically created for you to the HTML document
-document.body.appendChild(app.view);
+const gameElement = document.getElementById('game');
+gameElement.innerHTML = '';
+gameElement.appendChild(app.view);
+
+let rectangle = new PIXI.Graphics();
+rectangle.lineStyle(4, 0xFF3300, 1);
+rectangle.beginFill(0x66DDFF);
+rectangle.drawRect(0, 0, 40, 64);
+rectangle.endFill();
+rectangle.x = 170;
+rectangle.y = 170;
+app.stage.addChild(rectangle);
