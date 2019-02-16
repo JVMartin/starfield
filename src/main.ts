75b52fce76718df855ee6b55a1913f6b8b836cef
Jacob Martin <mindnumb@gmail.com>
2019-02-15 23:45:22 -0800

    Simple loop

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,23 @@ gameElement.appendChild(app.view);
 let rectangle = new PIXI.Graphics();
 rectangle.lineStyle(4, 0xFF3300, 1);
 rectangle.beginFill(0x66DDFF);
-rectangle.drawRect(0, 0, 40, 64);
+rectangle.drawRect(0, 0, 40, 40);
 rectangle.endFill();
 rectangle.x = 170;
 rectangle.y = 170;
 app.stage.addChild(rectangle);
+
+function gameLoop() {
+  // Call this `gameLoop` function on the next screen refresh
+  // (which happens 60 times per second)
+  requestAnimationFrame(gameLoop);
+
+  //Move the cat
+  rectangle.x += 1;
+  if (rectangle.x > 256) {
+    rectangle.x = 0;
+  }
+}
+
+//Start the loop
+gameLoop();
