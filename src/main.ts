04ed42e0e15c3972c57846f9c46f8e7b7fe8ddc9
Jacob Martin <mindnumb@gmail.com>
2019-02-16 00:41:17 -0800

    ws

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { PixiApplication } from './services/PixiApplication';
 import { Game } from './services/Game';
 
 (() => {
-  const pixiApplication = new PixiApplication(600, 600);
-  pixiApplication.inject();
+    const pixiApplication = new PixiApplication(600, 600);
+    pixiApplication.inject();
 
-  const game = new Game(pixiApplication);
-  game.populate();
-  game.run();
+    const game = new Game(pixiApplication);
+    game.populate();
+    game.run();
 })();
