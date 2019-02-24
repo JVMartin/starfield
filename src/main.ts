3f77600fdefc7732881c0dcb8ede636d2b688800
Jacob Martin <mindnumb@gmail.com>
2019-02-24 13:39:35 -0800

    Docs

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,20 @@ import { PixiApplication } from './services/PixiApplication';
 import { Slider } from './widgets/Slider';
 
 ((): void => {
+    // -------------------------------------------------
+    // Create the Pixi application
+    // -------------------------------------------------
     const pixiApplication: PixiApplication = new PixiApplication(700, 700);
     pixiApplication.replaceInDOM();
 
+    // -------------------------------------------------
+    // Create widgets for user input
+    // -------------------------------------------------
     const starsSlider: Slider = new Slider('stars');
 
+    // -------------------------------------------------
+    // Create our game and set it in motion
+    // -------------------------------------------------
     const game: Game = new Game(pixiApplication, starsSlider);
     game.initialize();
     game.run();
